d4c0c0b9fb31dd3fed0d0516c152129d467d768f
makutak <takkouno@gmail.com>
2019-01-28 23:40:31 +0900

    initial

---
+++ test/clj_echo_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-echo-server.core-test
+  (:require [clojure.test :refer :all]
+            [clj-echo-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
